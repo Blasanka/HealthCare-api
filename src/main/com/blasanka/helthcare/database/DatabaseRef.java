e19e1126ab054bc60ec5d657533b8b92cd4ce87e
blasanka <blasanka95@gmail.com>
2020-04-19 23:13:05 +0530

    user service also added as separate branch

---
+++ src/main/com/blasanka/helthcare/database/DatabaseRef.java
@@ -0,0 +1,224 @@
+package com.blasanka.helthcare.database;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import com.blasanka.helthcare.models.LoginSession;
+import com.blasanka.helthcare.models.User;
+import com.blasanka.helthcare.models.UserRole;
+
+public class DatabaseRef {
+	
+	private Connection connection = null;
+	
+	public DatabaseRef() {
+		String url = "jdbc:mysql://localhost:3306/helth_care?useUnicode=true"
+				+ "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		String username = "root";
+		String password = "";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(url, username, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Map<Long, User> getUsers(long id) {
+		
+		Map<Long, User> users = new HashMap<>();
+		String sql = "SELECT * FROM users u LEFT JOIN role r ON r.role_id = u.role_id";
+		
+		Statement statement;
+		try {
+			statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+			while (rs.next()) {
+				User user = new User(
+					rs.getLong(1),
+					rs.getString(2),
+					rs.getString(3),
+					rs.getString(4),
+					rs.getDate(6),
+					rs.getDate(7),
+					new UserRole(rs.getLong(8), rs.getString(9))
+				);
+				users.put(user.getUserId(), user);
+			}
+		    rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return users;
+	}
+
+	public User getUser(long id) {
+		
+		User user = new User();
+		String sql = "SELECT * FROM users u LEFT JOIN role r ON r.role_id = u.role_id WHERE u.user_id = ? ";
+		
+		PreparedStatement statement;
+		try {
+			statement = connection.prepareStatement(sql);
+			statement.setLong(1, id);
+			ResultSet rs = statement.executeQuery();
+			if (rs.next()) {
+				user = new User(
+					rs.getLong(1),
+					rs.getString(2),
+					rs.getString(3),
+					rs.getString(4),
+					rs.getDate(6),
+					rs.getDate(7),
+					new UserRole(rs.getLong(8), rs.getString(9))
+				);
+			}
+		    rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return user;
+	}
+	
+	public LoginSession getUserByCredentials(String email, String password) {
+		String sql = "SELECT * FROM users u LEFT JOIN role r ON r.role_id = u.role_id"
+				+ " LEFT JOIN logins l ON l.user_id = u.user_id"
+				+ " WHERE u.email = ? AND u.password = ?";
+
+		PreparedStatement statement;
+		LoginSession session = new LoginSession();
+		try {
+			statement = connection.prepareStatement(sql);
+			statement.setString(1, email);
+			statement.setString(2, password);
+			ResultSet rs = statement.executeQuery();
+			if (rs.next()) {
+				User user = new User(
+					rs.getLong(1),
+					rs.getString(2),
+					rs.getString(3),
+					rs.getString(4),
+					rs.getDate(6),
+					rs.getDate(7),
+					new UserRole(rs.getLong(8), rs.getString(9))
+				);
+				session = createLoginSession(user);
+				session.setUser(user);
+			}
+		    rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return session;
+	}
+	
+	public int addUser(User user) {
+		String sql = "INSERT INTO users(email, username, password, role_id, created_at) VALUES(?,?,?,?,?)";
+		
+		PreparedStatement statement;
+		int affectedRows = 0;
+		try {
+			statement = connection.prepareStatement(sql);
+			statement.setString(1, user.getEmail());
+			statement.setString(2, user.getUsername());
+			statement.setString(3, user.getPassword());
+			statement.setLong(4, user.getRoleId());
+			statement.setDate(5, new Date(System.currentTimeMillis()));
+			affectedRows = statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return affectedRows;
+	}
+
+	public int updateUser(User user) {
+		String sql = "UPDATE users SET"
+				+ " email = ?,"
+				+ " username = ?,"
+				+ " password = ?"
+				+ " WHERE user_id = ?";
+		
+		PreparedStatement statement;
+		int affectedRows = 0;
+		try {
+			statement = connection.prepareStatement(sql);
+			statement.setString(1, user.getEmail());
+			statement.setString(2, user.getUsername());
+			statement.setString(3, user.getPassword());
+			statement.setLong(4, user.getUserId());
+			affectedRows = statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return affectedRows;
+	}
+
+	public int deleteUser(long id) {
+		String sql = "DELETE FROM users WHERE user_id = ?";
+		
+		PreparedStatement statement;
+		int affectedRows = 0;
+		try {
+			statement = connection.prepareStatement(sql);
+			statement.setLong(1, id);
+			affectedRows = statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return affectedRows;
+	}
+	
+	public LoginSession createLoginSession(User user) {
+		String sql = "INSERT INTO logins(user_id, token) VALUES(?,?)";
+		
+		PreparedStatement statement;
+		LoginSession loginSession = new LoginSession();
+		try {
+			statement = connection.prepareStatement(sql);
+			statement.setLong(1, user.getUserId());
+			String token = UUID.randomUUID().toString()+"."+System.currentTimeMillis();
+			statement.setString(2, token);
+			int rs = statement.executeUpdate();
+			if (rs == 1) {
+				loginSession.setToken(token);
+				loginSession.setTimestamp(new Timestamp(System.currentTimeMillis()));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return loginSession;
+	}
+	
+	public boolean validateToken(String token) {
+		String sql = "SELECT token FROM logins WHERE token = ?";
+
+		PreparedStatement statement;
+		boolean success = false;
+		try {
+			statement = connection.prepareStatement(sql);
+			statement.setString(1, token);
+			ResultSet rs = statement.executeQuery();
+			if (rs.next() && rs.getString(1) != null) success = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return success;
+	}
+}
---
+++ src/main/com/blasanka/helthcare/resources/UserResource.java
@@ -0,0 +1,109 @@
+package com.blasanka.helthcare.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
+
+import com.blasanka.helthcare.controllers.UserController;
+import com.blasanka.helthcare.exceptions.UnAuthorizedException;
+import com.blasanka.helthcare.models.User;
+import com.blasanka.helthcare.models.LoginResult;
+import com.blasanka.helthcare.models.LoginSession;
+import com.blasanka.helthcare.resources.beans.UserFilterBean;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource {
+
+	UserController controller = new UserController();
+	
+	@GET
+	public List<User> getUsers(@BeanParam UserFilterBean bean) {
+		if (controller.validateToken(bean.getxAuthorization())) {
+			return controller.getUsers("user", bean.getId());
+		} else {
+//			return new ArrayList<User>();
+			throw new UnAuthorizedException("Authentication failed");
+		}
+	}
+	
+	@GET
+	@Path("/{userId}")
+	public Response getUser(@PathParam("userId") long id, @BeanParam UserFilterBean bean) {
+		if (controller.validateToken(bean.getxAuthorization())) {
+			User user = controller.getUser(id);
+			if (user.getUserId() == 0) throw new BadRequestException();
+			return Response.status(Status.CREATED).entity(user).build();
+		} else {
+			throw new UnAuthorizedException("Authentication failed");
+//			return Response.status(Status.UNAUTHORIZED).entity(null).build();
+		}
+	}
+	
+	@POST
+	public Response addUser(User user, @Context UriInfo info, @BeanParam UserFilterBean bean) {
+		user.setRoleId(bean.getRoleId());
+		int count = controller.addUser(user);
+		if (count == 0) throw new BadRequestException();
+		URI uri = info.getAbsolutePathBuilder().path(String.valueOf(count)).build();
+		return Response.created(uri).status(Status.CREATED).entity(count).build();
+	}
+	
+	@PUT
+	@Path("/{userId}")
+	public Response updateUser(@PathParam("userId") long id, @HeaderParam("x-authorization") String xAuth, User user) {
+		if (controller.validateToken(xAuth)) {
+			user.setUserId(id);
+			int count = controller.updateUser(user);
+			if (count == 0) throw new BadRequestException();
+			return Response.status(Status.NO_CONTENT).entity(count).build();
+		} else {
+			throw new UnAuthorizedException("Authentication failed");
+		}
+	}
+	
+	@DELETE
+	@Path("/{userId}")
+	public Response deleteUser(@PathParam("userId") long id, @HeaderParam("x-authorization") String xAuth) {
+		if (controller.validateToken(xAuth)) {
+			int count = controller.removeUser(id);
+			if (count == 0) throw new BadRequestException();
+			return Response.status(Status.NO_CONTENT).entity(count).build();
+		} else {
+			throw new UnAuthorizedException("Authentication failed");
+		}
+	}
+
+	
+	@POST
+	@Path("/login")
+	public Response loginUser(User user, @Context UriInfo info) {
+		LoginResult result = controller.getUserByCredentials(user.getEmail(), user.getPassword());
+		
+		if (result.getErrors().size() > 0) {
+			return Response.status(Status.UNAUTHORIZED).entity(result.getErrors()).build();
+		} else {
+			LoginSession ls = result.getLoginSession();
+			URI uri = info.getAbsolutePathBuilder().path(String.valueOf(ls.getUser().getUserId())).build();
+			return Response.created(uri).status(Status.OK).entity(ls).build();
+		}
+	}
+
+}
