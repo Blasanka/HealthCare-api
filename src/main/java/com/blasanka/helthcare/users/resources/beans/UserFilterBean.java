ea9706d15af7c95411d95778125d8d6aeb065f71
blasanka <blasanka95@gmail.com>
2020-04-19 22:38:03 +0530

    user service merged to be monolothics arch

---
+++ src/main/java/com/blasanka/helthcare/users/resources/beans/UserFilterBean.java
@@ -0,0 +1,32 @@
+package com.blasanka.helthcare.users.resources.beans;
+
+import javax.ws.rs.HeaderParam;
+
+
+public class UserFilterBean {
+
+	@HeaderParam("id") long id;
+	@HeaderParam("role_id") long roleId;
+	@HeaderParam("x-authorization") String xAuthorization;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public long getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(long roleId) {
+		this.roleId = roleId;
+	}
+	public String getxAuthorization() {
+		return xAuthorization;
+	}
+	public void setxAuthorization(String xAuthorization) {
+		this.xAuthorization = xAuthorization;
+	}
+
+}
